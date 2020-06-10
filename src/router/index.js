235c3a20ee009fb3dc3086531e40676744003d32
zhoujian <758507292@qq.com>
2020-06-10 11:47:49 +0800

    feat: 初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,157 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/**
+ *
+ * 当设置 true 的时候该路由不会在侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1
+   hidden: true // (默认 false)
+
+ * 当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
+   redirect: 'noRedirect'
+
+ * 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
+ * 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
+ * 若你想不管路由下面的 children 声明的个数都显示你的根路由
+ * 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
+   alwaysShow: true
+
+ * name: 'router-name' // 设定路由的名字，一定要填写! 不然使用<keep-alive>时会出现各种问题
+
+  meta: {
+    roles: ['admin', 'editor'] // 设置该路由进入的权限，支持多个权限叠加
+    title: 'title' // 设置该路由在侧边栏和面包屑中展示的名字
+    icon: 'svg-name' // 设置该路由的图标
+    noCache: true // 如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+    breadcrumb: false //  如果设置为false，则不会在breadcrumb面包屑中显示(默认 true)
+    affix: true // 若果设置为true，它则会固定在tags-view中(默认 false)
+
+    // 当路由设置了该属性，则会高亮相对应的侧边栏。
+    // 这在某些场景非常有用，比如：一个文章的列表页路由为：/article/list
+    // 点击文章进入文章详情页，这时候路由为/article/1，但你想在侧边栏高亮文章列表的路由，就可以进行如下设置
+    activeMenu: '/article/list'
+  }
+*/
+
+/**
+ * constantRoutes
+ * 这里的路由是所有权限都显示的，不受权限控制
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
+  }
+]
+
+/**
+ * asyncRoutes
+ * 这里的路由会根据用户的角色动态添加
+ */
+export const asyncRoutes = [
+  {
+    path: '/nested',
+    component: Layout,
+    redirect: '/nested/menu1',
+    name: 'Nested',
+    meta: {
+      title: '这个菜单是和权限有关的',
+      icon: 'nested',
+      roles: ['editor']
+    },
+    children: [
+      {
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu1' },
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'Menu1-2',
+            meta: { title: 'Menu1-2',  roles: ['admin'] },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu1-3', roles: ['admin'] }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: 'menu2' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  showSettings: true,
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
