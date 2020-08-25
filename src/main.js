c5781fb1ef94807c7d00a4542bd3f2ce01f4085f
zhoujian <758507292@qq.com>
2020-08-25 15:33:07 +0800

    feat: 测试环境才开启mock服务

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ import '@/permission' // permission control
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
   const { mockXHR } = require('../mock')
   mockXHR()
 }
